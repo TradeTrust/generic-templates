2e27b5616f344aa3b7c7f33cded168115d000350
Raymond Yeh <ray@geek.sg>
2020-07-28 16:22:16 +0800

    fix: better style

--- src/templates/coveringLetter/customTemplate.tsx
+++ src/templates/coveringLetter/customTemplate.tsx
@@ -2,6 +2,19 @@ import React, { FunctionComponent } from "react";
 import { TemplateProps } from "@govtechsg/decentralized-renderer-react-components";
 import { CoveringLetter } from "./sample";
 import { PrintWatermark } from "../../core/PrintWatermark";
+import { css } from "@emotion/core";
+
+const container = css`
+  font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+  color: #4e4e50;
+  text-align: center;
+  width: 100%;
+  height: 100vh;
+  max-height: 1000px;
+  max-width: 980px;
+  position: relative;
+  margin: 0 auto;
+`;
 
 export const CustomTemplate: FunctionComponent<TemplateProps<CoveringLetter> & { className?: string }> = ({
   document,
@@ -9,7 +22,12 @@ export const CustomTemplate: FunctionComponent<TemplateProps<CoveringLetter> & {
 }) => {
   const { logo, title, description, backgroundColor, titleColor, descriptionColor } = document;
   return (
-    <div className={className} id="custom-template" style={{ backgroundColor, textAlign: "center", padding: 20 }}>
+    <div
+      css={container}
+      className={className}
+      id="custom-template"
+      style={{ backgroundColor, textAlign: "center", padding: 20 }}
+    >
       <PrintWatermark />
       {logo ? (
         <img src={logo} style={{ maxWidth: 600, width: "100%", paddingTop: 50, paddingBottom: 50 }} />
