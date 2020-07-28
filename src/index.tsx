3fa5cab0e8ff4a95e03dab221442ce0380b29f5e
Raymond Yeh <ray@geek.sg>
2020-07-28 16:12:07 +0800

    chore: removed attachment rendering

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { FramedDocumentRenderer } from "@govtechsg/decentralized-renderer-react-components";
+import { FramedDocumentRenderer, noAttachmentRenderer } from "@govtechsg/decentralized-renderer-react-components";
 import { registry } from "./templates";
 
-ReactDOM.render(<FramedDocumentRenderer templateRegistry={registry} />, document.getElementById("root"));
+ReactDOM.render(
+  <FramedDocumentRenderer templateRegistry={registry} attachmentToComponent={noAttachmentRenderer} />,
+  document.getElementById("root")
+);
