c7c989773331be1f256ccdd58a890c3e598ecba5
863303 <a863303@ambsg000076.lan>
2020-09-11 10:03:50 +0800

    chore: updating the naming to generic template

--- application/webpack.config.js
+++
@@ -1,6 +0,0 @@
-const webpackConfig = require("../webpack.config");
-
-webpackConfig.entry.app = ["./application/index.tsx"];
-webpackConfig.devServer.port = 3010;
-
-module.exports = webpackConfig;
