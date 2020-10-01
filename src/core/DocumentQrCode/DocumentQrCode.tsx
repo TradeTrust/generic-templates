c15173f0f0592c0a3da31b221c2f777f42afcd1a
Sim Boon Long <simboonlong@gmail.com>
2020-10-01 18:32:26 +0800

    feat: qr logo

--- src/core/DocumentQrCode/DocumentQrCode.tsx
+++ src/core/DocumentQrCode/DocumentQrCode.tsx
@@ -1,16 +1,54 @@
-import QRCode from "qrcode.react";
-import React, { FunctionComponent } from "react";
+import QRCode, { ImageSettings } from "qrcode.react";
+import React, { FunctionComponent, useState } from "react";
 import "./style.css";
 
 interface DocumentQrCode {
   url: string;
-  size?: number;
+  logo?: string;
 }
 
-export const DocumentQrCode: FunctionComponent<DocumentQrCode> = ({ url, size = 250 }) => (
-  <div className="show-print">
-    <QRCode value={url} size={size} />
-    <div style={{ fontSize: 32, marginLeft: 64 }}>Scan the QR code with a QR code scanner device.</div>
-    <div className="genterated-text">Automatically Generated</div>
-  </div>
-);
+// https://stackoverflow.com/questions/3971841/how-to-resize-images-proportionally-keeping-the-aspect-ratio
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const getDimensions = ({
+  width,
+  height,
+  maxWidth,
+  maxHeight
+}: {
+  width: number;
+  height: number;
+  maxWidth: number;
+  maxHeight: number;
+}) => {
+  const ratio = Math.min(maxWidth / width, maxHeight / height);
+  return { width: Math.round(width * ratio), height: Math.round(height * ratio) };
+};
+
+export const DocumentQrCode: FunctionComponent<DocumentQrCode> = ({ url, logo }) => {
+  const [imageSettings, setImageSettings] = useState<ImageSettings>();
+
+  if (logo) {
+    const img: HTMLImageElement = new Image();
+    img.src = logo;
+    img.onload = () => {
+      const logoSize = getDimensions({ width: img.width, height: img.height, maxWidth: 100, maxHeight: 100 });
+
+      setImageSettings({
+        src: logo,
+        x: undefined,
+        y: undefined,
+        height: logoSize.height,
+        width: logoSize.width,
+        excavate: true
+      });
+    };
+  }
+
+  return (
+    <div className="show-print">
+      <QRCode value={url} size={400} imageSettings={imageSettings} />
+      <div style={{ fontSize: 32, marginLeft: 64 }}>Scan the QR code with a QR code scanner device.</div>
+      <div className="genterated-text">Automatically Generated</div>
+    </div>
+  );
+};
