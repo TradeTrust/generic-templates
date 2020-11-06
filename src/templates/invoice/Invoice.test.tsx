69235c34ed27dd3a00020a4f1463c36cd081ea74
Matthea Loo <dhemutton@users.noreply.github.com>
2020-11-06 14:57:03 +0800

    feat: add subtotal

--- src/templates/invoice/Invoice.test.tsx
+++ src/templates/invoice/Invoice.test.tsx
@@ -17,29 +17,7 @@ describe("covering Letter", () => {
     expect(getByText("DESCRIPTION")).toBeInTheDocument();
     expect(getByText("QTY")).toBeInTheDocument();
     expect(getByText("UNIT PRICE")).toBeInTheDocument();
-    expect(getByText("AMOUNT")).toBeInTheDocument();
-
-    //dynamic texts
-    // expect(getByText("2034")).toBeInTheDocument();
-    // expect(getByText("21 Feb 2018")).toBeInTheDocument();
-    // expect(getByText("564")).toBeInTheDocument();
-    // expect(getByText("Due Upon Receipt")).toBeInTheDocument();
-    // expect(getByText("ABC Company")).toBeInTheDocument();
-    // expect(getByText("Level 1, Industry Offices")).toBeInTheDocument();
-    // expect(getByText("Singapore, 123456")).toBeInTheDocument();
-    // expect(getByText("60305029")).toBeInTheDocument();
-    // expect(getByText("DEF Company")).toBeInTheDocument();
-    // expect(getByText("Level 2, Industry Offices")).toBeInTheDocument();
-    // expect(getByText("Singapore, 612345")).toBeInTheDocument();
-    // expect(getByText("61204028")).toBeInTheDocument();
-    // expect(getByText("James Lee")).toBeInTheDocument();
-    // expect(getByText("def@company.com")).toBeInTheDocument();
-    // expect(getByText("Service Fee")).toBeInTheDocument();
-    // expect(getAllByText("1")).toHaveLength(2);
-    // expect(getAllByText("200")).toHaveLength(2);
-    // expect(getByText("5")).toBeInTheDocument();
-    // expect(getByText("75")).toBeInTheDocument();
-    // expect(getByText("375")).toBeInTheDocument();
-    // expect(getAllByText("50")).toHaveLength(2);
+    expect(getByText("SUBTOTAL")).toBeInTheDocument();
+    expect(getByText("BALANCE DUE")).toBeInTheDocument();
   });
 });
--- src/templates/invoice/Invoice.tsx
+++ src/templates/invoice/Invoice.tsx
@@ -1,4 +1,3 @@
-import { css } from "@emotion/core";
 import { TemplateProps } from "@govtechsg/decentralized-renderer-react-components";
 import React, { FunctionComponent } from "react";
 import { Invoice } from "./types";
@@ -21,16 +20,22 @@ const Container = styled.div`
     font-weight: 700;
   }
 
+  h4 {
+    font-weight: 700;
+  }
+
   .bg-blue {
     background-color: #4172af;
     color: #fff;
   }
 
-  /*
   th {
     background-color: #4172af;
     color: white;
   }
+
+  /*
+
 
   .table-right {
     width: 40%;
@@ -72,27 +77,14 @@ const Container = styled.div`
 `;
 
 export const InvoiceTemplate: FunctionComponent<TemplateProps<Invoice>> = ({ document }) => {
-  const { id, date, customerId, terms, billFrom, billTo, billableItems } = document;
+  const { id, date, customerId, terms, billFrom, billTo, billableItems, subtotal, tax, taxTotal, total } = document;
 
   return (
-    <Container className="p-4">
+    <Container className="p-4 container">
       <div className="flex flex-wrap">
-        <div className="w-full md:w-auto md:ml-auto md:order-2 mb-4 md:mb-0">
-          <h1>INVOICE</h1>
-        </div>
-        <div className="w-full md:w-1/2 md:order-1 mb-4 md:mb-0">
-          <h2>{billFrom?.name}</h2>
-          <h3>{billFrom?.streetAddress}</h3>
-          <h3>
-            {billFrom?.city}
-            {`, ${billFrom?.postalCode}`}
-          </h3>
-          <h3>{billFrom?.phoneNumber}</h3>
-        </div>
-      </div>
-      <div className="flex flex-wrap text-center mb-4 md:mb-0">
-        <div className="w-full md:w-1/2 md:ml-auto">
-          <div className="flex flex-wrap bg-blue">
+        <div className="w-full md:w-5/12 md:ml-auto md:order-2">
+          <h1 className="text-right">INVOICE</h1>
+          <div className="flex flex-wrap bg-blue text-center">
             <div className="w-1/2 p-1">
               <p>INVOICE #</p>
             </div>
@@ -100,135 +92,93 @@ export const InvoiceTemplate: FunctionComponent<TemplateProps<Invoice>> = ({ doc
               <p>DATE</p>
             </div>
           </div>
-          <div className="flex flex-wrap">
+          <div className="flex flex-wrap text-center">
             <div className="w-1/2 p-1">
               <p>{id}</p>
             </div>
-            <div className="w-1/2 p-1">{date && <p>{format(new Date(date), "d MMM yyyy")}</p>}</div>
+            <div className="w-1/2 p-1">
+              <p>{date && format(new Date(date), "d MMM yyyy")}</p>
+            </div>
+          </div>
+          <div className="flex flex-wrap bg-blue text-center">
+            <div className="w-1/2 p-1">
+              <p>CUSTOMER ID</p>
+            </div>
+            <div className="w-1/2 p-1">
+              <p>TERMS</p>
+            </div>
+          </div>
+          <div className="flex flex-wrap text-center">
+            <div className="w-1/2 p-1">
+              <p>{customerId}</p>
+            </div>
+            <div className="w-1/2 p-1">{terms}</div>
+          </div>
+        </div>
+        <div className="w-full md:w-5/12 md:order-1 mb-4 md:mb-0">
+          <h2>{billFrom?.name}</h2>
+          <h3>{billFrom?.streetAddress}</h3>
+          <h3>
+            {billFrom?.city}
+            {`, ${billFrom?.postalCode}`}
+          </h3>
+          <h3>{billFrom?.phoneNumber}</h3>
+          <div className="flex flex-wrap bg-blue">
+            <div className="p-1 pl-2">
+              <p>BILL TO</p>
+            </div>
+          </div>
+          <div className="flex flex-wrap">
+            <div className="p-1">
+              <p>{billTo?.name}</p>
+              <p>{billTo?.company.name}</p>
+              <p>{billTo?.company.streetAddress}</p>
+              <p>
+                {billTo?.company.city}
+                {`, ${billTo?.company.postalCode}`}
+              </p>
+              <p>{billTo?.company.phoneNumber}</p>
+              <p>{billTo?.email}</p>
+            </div>
           </div>
         </div>
       </div>
-      <table class="table-auto">
+      <div className="flex flex-wrap text-center mb-4 md:mb-0" />
+      <table className="table-auto w-full">
         <thead>
           <tr>
-            <th class="px-4 py-2">Title</th>
-            <th class="px-4 py-2">Author</th>
-            <th class="px-4 py-2">Views</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td class="border px-4 py-2">Intro to CSS</td>
-            <td class="border px-4 py-2">Adam</td>
-            <td class="border px-4 py-2">858</td>
-          </tr>
-          <tr class="bg-gray-100">
-            <td class="border px-4 py-2">
-              A Long and Winding Tour of the History of UI Frameworks and Tools and the Impact on Design
-            </td>
-            <td class="border px-4 py-2">Adam</td>
-            <td class="border px-4 py-2">112</td>
-          </tr>
-          <tr>
-            <td class="border px-4 py-2">Intro to JavaScript</td>
-            <td class="border px-4 py-2">Chris</td>
-            <td class="border px-4 py-2">1,280</td>
-          </tr>
-        </tbody>
-      </table>
-      <table className="table-right" style={{ top: "180px" }}>
-        <thead>
-          <tr>
-            <th scope="col">CUSTOMER ID</th>
-            <th scope="col">TERMS</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{customerId}</td>
-            <td>{terms}</td>
-          </tr>
-        </tbody>
-      </table>
-      <table className="table-left">
-        <thead>
-          <tr>
-            <th scope="col">BILL TO</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>{billTo?.name}</td>
-          </tr>
-          <tr>
-            <td>{billTo?.company.name}</td>
-          </tr>
-          <tr>
-            <td>{billTo?.company.streetAddress}</td>
-          </tr>
-          <tr>
-            <td>
-              {billTo?.company.city}
-              {`, ${billTo?.company.postalCode}`}
-            </td>
-          </tr>
-          <tr>
-            <td>{billTo?.company.phoneNumber}</td>
-          </tr>
-          <tr>
-            <td>{billTo?.email}</td>
-          </tr>
-        </tbody>
-      </table>
-      <table className="table-full">
-        <thead>
-          <tr>
-            <th scope="col">DESCRIPTION</th>
-            <th scope="col">QTY</th>
-            <th scope="col">UNIT PRICE</th>
-            <th scope="col">AMOUNT</th>
+            <th className="px-4 py-2">DESCRIPTION</th>
+            <th className="px-4 py-2">QTY</th>
+            <th className="px-4 py-2">UNIT PRICE</th>
+            <th className="px-4 py-2">AMOUNT</th>
           </tr>
         </thead>
         <tbody>
           {billableItems?.map((item, index) => {
             return (
               <tr key={index}>
-                <td className="border">{item.description}</td>
-                <td
-                  className="border"
-                  style={{
-                    textAlign: "center"
-                  }}
-                >
-                  {item.quantity}
-                </td>
-                <td
-                  className="border"
-                  style={{
-                    textAlign: "right"
-                  }}
-                >
-                  {item.unitPrice}
-                </td>
-                <td
-                  className="border"
-                  style={{
-                    borderStyle: "none solid solid solid",
-                    textAlign: "right"
-                  }}
-                >
-                  {item.amount}
-                </td>
+                <td className="border px-4 py-2">{item.description}</td>
+                <td className="border px-4 py-2 text-center">{item.quantity}</td>
+                <td className="border px-4 py-2 text-right">{item.unitPrice}</td>
+                <td className="border px-4 py-2 text-right"> {item.amount}</td>
               </tr>
             );
           })}
         </tbody>
       </table>
-      <div className="total">
-        <h4>SUBTOTAL</h4>
-        <h4>TAX</h4>
-        <hr />
-        <h4>BALANCE DUE</h4>
+      <div className="w-full md:w-5/12 md:ml-auto mt-2 flex text-right">
+        <div className="w-full md:w-1/2 border-bottom">
+          <h4>SUBTOTAL</h4>
+          <h4>{`TAX (${tax}%)`}</h4>
+          <hr />
+          <h4>BALANCE DUE</h4>
+        </div>
+        <div className="w-full md:w-1/2">
+          <p>{subtotal}</p>
+          <p>{taxTotal}</p>
+          <hr />
+          <p className="font-bold">{total}</p>
+        </div>
       </div>
     </Container>
   );
