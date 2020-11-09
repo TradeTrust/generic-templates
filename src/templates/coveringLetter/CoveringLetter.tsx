51fffda095414697bd1b798e425bd41124dd098a
Sim Boon Long <simboonlong@gmail.com>
2020-11-09 16:49:49 +0800

    fix: cover letter height

--- src/templates/coveringLetter/CoveringLetter.tsx
+++ src/templates/coveringLetter/CoveringLetter.tsx
@@ -1,70 +1,60 @@
-import { css } from "@emotion/core";
 import { TemplateProps } from "@govtechsg/decentralized-renderer-react-components";
 import React, { FunctionComponent } from "react";
 import { DocumentQrCode } from "../../core/DocumentQrCode";
 import { PrintWatermark } from "../../core/PrintWatermark";
 import { CoveringLetter } from "./types";
+import styled from "@emotion/styled";
 
-const container = css`
+const Container = styled.div`
   font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
-  color: #4e4e50;
   width: 100%;
-  height: 100vh;
-  max-height: 1000px;
-  max-width: 980px;
-  position: relative;
-  margin: 0 auto;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
+  color: #4e4e50;
+  min-height: 600px; // take note that logo image's height was never accounted in set iframe height, hence need a min-height in px, avoid using vh units
+
+  .logo {
+    max-width: 240px;
+  }
+
+  h1 {
+    font-size: 32px;
+  }
 `;
 
-export const CoveringLetterTemplate: FunctionComponent<TemplateProps<CoveringLetter> & { className?: string }> = ({
-  document,
-  className = ""
-}) => {
+export const CoveringLetterTemplate: FunctionComponent<TemplateProps<CoveringLetter>> = ({ document }) => {
   const { logo, title, remarks, backgroundColor, titleColor, remarksColor } = document;
   const qrCodeUrl = document?.links?.self.href;
 
   return (
-    <div>
-      <div
-        css={container}
-        className={className}
-        id="covering-letter-template"
-        style={{ backgroundColor, textAlign: "center", padding: 20 }}
-      >
+    <div style={{ backgroundColor }}>
+      <Container className="p-4 mx-auto container">
         <PrintWatermark />
-        {logo ? <img src={logo} style={{ maxWidth: 250, width: "100%" }} /> : <div style={{ height: 300 }} />}
-        {title && <h1 style={{ color: titleColor, display: "flex", alignItems: "flex-start" }}>{title}</h1>}
+        {logo && <img className="logo my-4" src={logo} />}
+        {title && (
+          <h1 className="font-bold" style={{ color: titleColor }}>
+            {title}
+          </h1>
+        )}
         {remarks && (
-          <div>
+          <div className="my-4">
             <div
+              className="font-bold"
               style={{
-                color: remarksColor,
-                fontWeight: "bold",
-                textAlign: "left",
-                marginTop: 16,
-                marginBottom: 16
+                color: remarksColor
               }}
             >
               Remarks:
             </div>
             <div
               style={{
-                color: remarksColor,
-                whiteSpace: "pre-wrap",
-                display: "flex",
-                alignItems: "flex-start",
-                textAlign: "left"
+                color: remarksColor
               }}
             >
               {remarks}
             </div>
           </div>
         )}
-      </div>
-      {qrCodeUrl && <DocumentQrCode url={qrCodeUrl} />}
+        {qrCodeUrl && <DocumentQrCode url={qrCodeUrl} />}
+      </Container>
     </div>
   );
 };
