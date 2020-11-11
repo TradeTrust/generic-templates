b92c8e30bd219d87aa89787c2271fc7daf505635
Sim Boon Long <simboonlong@gmail.com>
2020-11-11 10:01:36 +0800

    fix: table responsive, tweak spacing

--- src/templates/invoice/Invoice.tsx
+++ src/templates/invoice/Invoice.tsx
@@ -58,54 +58,56 @@ export const InvoiceTemplate: FunctionComponent<TemplateProps<Invoice>> = ({ doc
   return (
     <Container className="p-4 mx-auto container" data-testid="invoice-template">
       <div className="flex flex-wrap">
-        <div className="w-full md:w-5/12 md:ml-auto md:order-2">
-          <h1 className="text-right">INVOICE</h1>
+        <div className="w-full md:w-5/12 mb-4 md:mb-0 md:ml-auto md:order-2">
+          <h1 className="md:text-right leading-none mb-6">INVOICE</h1>
           <div className="flex flex-wrap bg-blue text-center">
-            <div className="w-1/2 p-1">
+            <div className="w-1/2 py-1">
               <p>INVOICE #</p>
             </div>
-            <div className="w-1/2 p-1">
+            <div className="w-1/2 py-1">
               <p>DATE</p>
             </div>
           </div>
           <div className="flex flex-wrap text-center">
-            <div className="w-1/2 p-1">
+            <div className="w-1/2 py-1">
               <p>{id}</p>
             </div>
-            <div className="w-1/2 p-1">
+            <div className="w-1/2 py-1">
               <p>{date && format(new Date(date), "d MMM yyyy")}</p>
             </div>
           </div>
           <div className="flex flex-wrap bg-blue text-center">
-            <div className="w-1/2 p-1">
+            <div className="w-1/2 py-1">
               <p>CUSTOMER ID</p>
             </div>
-            <div className="w-1/2 p-1">
+            <div className="w-1/2 py-1">
               <p>TERMS</p>
             </div>
           </div>
           <div className="flex flex-wrap text-center">
-            <div className="w-1/2 p-1">
+            <div className="w-1/2 py-1">
               <p>{customerId}</p>
             </div>
-            <div className="w-1/2 p-1">{terms}</div>
+            <div className="w-1/2 py-1">{terms}</div>
           </div>
         </div>
-        <div className="w-full md:w-5/12 md:order-1 mb-4 md:mb-0">
-          <h2>{billFrom?.name}</h2>
-          <h3>{billFrom?.streetAddress}</h3>
-          <h3>
-            {billFrom?.city}
-            {billFrom?.postalCode && `, ${billFrom?.postalCode}`}
-          </h3>
-          <h3>{billFrom?.phoneNumber}</h3>
+        <div className="w-full md:w-5/12 md:order-1">
+          <div className="mb-4">
+            <h2>{billFrom?.name}</h2>
+            <h3>{billFrom?.streetAddress}</h3>
+            <h3>
+              {billFrom?.city}
+              {billFrom?.postalCode && `, ${billFrom?.postalCode}`}
+            </h3>
+            <h3>{billFrom?.phoneNumber}</h3>
+          </div>
           <div className="flex flex-wrap bg-blue">
-            <div className="p-1 pl-2">
+            <div className="pl-2">
               <p>BILL TO</p>
             </div>
           </div>
           <div className="flex flex-wrap">
-            <div className="p-1">
+            <div className="mb-4 py-2">
               <p>{billTo?.name}</p>
               <p>{billTo?.company.name}</p>
               <p>{billTo?.company.streetAddress}</p>
@@ -119,29 +121,31 @@ export const InvoiceTemplate: FunctionComponent<TemplateProps<Invoice>> = ({ doc
           </div>
         </div>
       </div>
-      <div className="flex flex-wrap text-center mb-4 md:mb-0" />
-      <table className="table-auto w-full table-responsive">
-        <thead>
-          <tr>
-            <th className="px-4 py-2">DESCRIPTION</th>
-            <th className="px-4 py-2">QTY</th>
-            <th className="px-4 py-2">UNIT PRICE</th>
-            <th className="px-4 py-2">AMOUNT</th>
-          </tr>
-        </thead>
-        <tbody>
-          {billableItems?.map((item, index) => {
-            return (
-              <tr key={index}>
-                <td className="border px-4 py-2">{item.description}</td>
-                <td className="border px-4 py-2 text-center">{item.quantity}</td>
-                <td className="border px-4 py-2 text-right">{item.unitPrice}</td>
-                <td className="border px-4 py-2 text-right"> {item.amount}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div className="flex flex-wrap mb-4 md:mb-0" />
+      <div className="table-responsive mb-4">
+        <table className="table-auto w-full border">
+          <thead>
+            <tr>
+              <th className="px-4 py-2 text-left">DESCRIPTION</th>
+              <th className="px-4 py-2 text-center">QTY</th>
+              <th className="px-4 py-2 text-right">UNIT PRICE</th>
+              <th className="px-4 py-2 text-right">AMOUNT</th>
+            </tr>
+          </thead>
+          <tbody>
+            {billableItems?.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td className="border px-4 py-2">{item.description}</td>
+                  <td className="border px-4 py-2 text-center">{item.quantity}</td>
+                  <td className="border px-4 py-2 text-right">{item.unitPrice}</td>
+                  <td className="border px-4 py-2 text-right"> {item.amount}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
       <div className="w-full md:w-5/12 md:ml-auto mt-2 flex text-right">
         <div className="w-full md:w-1/2 border-bottom">
           <h4>SUBTOTAL</h4>
