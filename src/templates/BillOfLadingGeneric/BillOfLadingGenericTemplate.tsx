89baf8df03be70fe495cbbe045ba1c950ad11224
ryanseee <50093641+ryanseee@users.noreply.github.com>
2021-02-03 14:45:48 +0800

    chore: added field labels (#18)

--- src/templates/BillOfLadingGeneric/BillOfLadingGenericTemplate.tsx
+++ src/templates/BillOfLadingGeneric/BillOfLadingGenericTemplate.tsx
@@ -45,37 +45,37 @@ export const BillOfLadingGenericTemplate: FunctionComponent<TemplateProps<BillOf
 
           <div className="flex">
             <div className="w-1/2 border-black border">
-              <div className="p-2">{field1 && <strong className="break-all">{field1}</strong>}</div>
+              <div className="p-2">Field 1: {field1 && <strong className="break-all">{field1}</strong>}</div>
             </div>
             <div className="w-1/2 border-black border">
-              <div className="p-2">{field2 && <strong className="break-all">{field2}</strong>}</div>
+              <div className="p-2">Field 2: {field2 && <strong className="break-all">{field2}</strong>}</div>
             </div>
             <div className="w-1/2 border-black border">
-              <div className="p-2">{field3 && <strong className="break-all">{field3}</strong>}</div>
-            </div>
-          </div>
-
-          <div className="flex">
-            <div className="w-1/2 border-black border">
-              <div className="p-2">{field4 && <strong className="break-all">{field4}</strong>}</div>
-            </div>
-            <div className="w-1/2 border-black border">
-              <div className="p-2">{field5 && <strong className="break-all">{field5}</strong>}</div>
-            </div>
-            <div className="w-1/2 border-black border">
-              <div className="p-2">{field6 && <strong className="break-all">{field6}</strong>}</div>
+              <div className="p-2">Field 3: {field3 && <strong className="break-all">{field3}</strong>}</div>
             </div>
           </div>
 
           <div className="flex">
             <div className="w-1/2 border-black border">
-              <div className="p-2">{field7 && <strong className="break-all">{field7}</strong>}</div>
+              <div className="p-2">Field 4: {field4 && <strong className="break-all">{field4}</strong>}</div>
             </div>
             <div className="w-1/2 border-black border">
-              <div className="p-2">{field8 && <strong className="break-all">{field8}</strong>}</div>
+              <div className="p-2">Field 5: {field5 && <strong className="break-all">{field5}</strong>}</div>
             </div>
             <div className="w-1/2 border-black border">
-              <div className="p-2">{field9 && <strong className="break-all">{field9}</strong>}</div>
+              <div className="p-2">Field 6: {field6 && <strong className="break-all">{field6}</strong>}</div>
+            </div>
+          </div>
+
+          <div className="flex">
+            <div className="w-1/2 border-black border">
+              <div className="p-2">Field 7: {field7 && <strong className="break-all">{field7}</strong>}</div>
+            </div>
+            <div className="w-1/2 border-black border">
+              <div className="p-2">Field 8: {field8 && <strong className="break-all">{field8}</strong>}</div>
+            </div>
+            <div className="w-1/2 border-black border">
+              <div className="p-2">Field 9: {field9 && <strong className="break-all">{field9}</strong>}</div>
             </div>
           </div>
         </div>
