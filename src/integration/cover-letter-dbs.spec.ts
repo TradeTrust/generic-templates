24fa2bd06fdb8544a174e06fad7ad045d0440be6
Sim Boon Long <simboonlong@gmail.com>
2021-06-02 11:22:37 +0800

    fix: omit percy

--- src/integration/cover-letter-dbs.spec.ts
+++ src/integration/cover-letter-dbs.spec.ts
@@ -7,7 +7,7 @@ const CoveringLetterTemplate = Selector("[data-testid='covering-letter-template'
 test("Covering Letter (DBS) is rendered correctly", async (t) => {
   await t.click(Selector("[data-testid='COVERING_LETTER']").withText("Covering Letter (DBS)"));
   await t.switchToIframe("#iframe");
-  await t.expect(CoveringLetterTemplate.visible).ok();
+  await t.expect(CoveringLetterTemplate.exists).ok();
   await t.expect(CoveringLetterTemplate.textContent).contains("Documents Bundle");
-  await t.expect(Selector('img[src="/static/images/logo-dbs.png"]')).ok();
+  await t.expect(Selector('img[src="/static/images/logo-dbs.png"]').exists).ok();
 });
