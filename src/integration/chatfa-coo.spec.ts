70ecdee18654a8bfb2040bb727a91b5bf680dae6
ChenRen <siachenren@gmail.com>
2021-09-20 14:55:02 +0800

    fix: integration test

--- src/integration/chatfa-coo.spec.ts
+++ src/integration/chatfa-coo.spec.ts
@@ -5,7 +5,7 @@ fixture("Chafta COO").page`http://localhost:3010`;
 const ChaftaCooTemplate = Selector("[data-testid='chafta-coo-template']");
 
 test("Chafta COO is rendered correctly", async (t) => {
-  await t.click(Selector("[data-testid='Chafta COO']"));
+  await t.click(Selector("[data-testid='Chafta COO V2']"));
   await t.switchToIframe("#iframe");
   await t.expect(ChaftaCooTemplate.visible).ok();
   await t.expect(ChaftaCooTemplate.textContent).contains("CERTIFICATE OF ORIGIN");
