40a47ba04be90b57b4227539632fb655d4dcbd79
Sim Boon Long <simboonlong@gmail.com>
2021-09-27 14:15:02 +0800

    fix: coo fields

--- src/templates/ChaftaCoo/ChaftaCooTemplate.stories.tsx
+++ src/templates/ChaftaCoo/ChaftaCooTemplate.stories.tsx
@@ -11,6 +11,10 @@ export default {
   },
 };
 
+export const ChaftaCooEmpty: FunctionComponent = () => {
+  return <ChaftaCooTemplate document={{} as any} handleObfuscation={() => {}} />; // when empty, visually should not show any dangling values
+};
+
 export const ChaftaCooV2: FunctionComponent = () => {
   return <ChaftaCooTemplate document={ChaftaCooSampleV2} handleObfuscation={() => {}} />;
 };
--- src/templates/ChaftaCoo/ChaftaCooTemplate.tsx
+++ src/templates/ChaftaCoo/ChaftaCooTemplate.tsx
@@ -31,6 +31,8 @@ type ObfuscationHandling = {
 type ChaftaCooDocumentWithObfuscation = ChaftaCooDocument & ObfuscationHandling;
 type ChaftaCooTemplateProps = TemplateProps<ChaftaCooDocumentSchema>;
 
+const UnderlineDashed: FunctionComponent<React.ReactNode> = () => <div className="border-dashed border-b-2 my-2" />;
+
 export const PrivacyButton: FunctionComponent<PrivacyButtonProps> = ({
   isPrivacyOn,
   handleObfuscation,
@@ -64,19 +66,19 @@ export const ExporterSection: FunctionComponent<ChaftaCooDocumentWithObfuscation
   const privacyPath = ["supplyChainConsignment.exporter"];
 
   return (
-    <div className="border p-2">
-      <div>
+    <div className="border p-2 flex-1">
+      <h5>
         1. Exporter’s name, address and country:{" "}
         <PrivacyButton isPrivacyOn={isPrivacyOn} handleObfuscation={handleObfuscation} paths={privacyPath} />
-      </div>
-      <div>{exporter?.name}</div>
-      <div>{postalAddress?.line1}</div>
-      <div>{postalAddress?.line2}</div>
-      <div>{postalAddress?.cityName}</div>
-      <div>
+      </h5>
+      <p>{exporter?.name}</p>
+      <p>{postalAddress?.line1}</p>
+      <p>{postalAddress?.line2}</p>
+      <p>{postalAddress?.cityName}</p>
+      <p>
         {postalAddress?.countrySubDivisionName} {postalAddress?.postcode} {postalAddress?.countryCode}
-      </div>
-      <div>ABN {getValue(exporter?.iD)}</div>
+      </p>
+      {exporter?.iD && <p>ABN {getValue(exporter?.iD)}</p>}
     </div>
   );
 };
@@ -95,33 +97,37 @@ export const ProducerSection: FunctionComponent<ChaftaCooDocumentWithObfuscation
   );
 
   return (
-    <div className="border p-2">
-      <div>
+    <div className="border p-2 flex-1">
+      <h5>
         2. Producer’s name and address (if known):{" "}
         <PrivacyButton isPrivacyOn={isPrivacyOn} handleObfuscation={handleObfuscation} paths={privacyPath} />
-      </div>
-      <div>{manufacturer?.name}</div>
-      <div>
+      </h5>
+      <p>{manufacturer?.name}</p>
+      <p>
         {postalAddress?.line1}
-        {", "}
-        {postalAddress?.cityName},
-      </div>
-      <div>
+        {postalAddress?.line1 && postalAddress?.cityName && `, `}
+        {postalAddress?.cityName}
+      </p>
+      <p>
         {postalAddress?.countrySubDivisionName} {postalAddress?.postcode} {postalAddress?.countryCode}
-      </div>
+      </p>
     </div>
   );
 };
 
 export const SummarySection: FunctionComponent<ChaftaCooDocument> = ({ iD }) => {
   return (
-    <div className="border p-2 h-full text-center">
-      <div>Certificate No.: {getValue(iD)}</div>
-      <div className="p-2">
-        <div>CERTIFICATE OF ORIGIN</div>
-        <div>Form for China-Australia Free Trade Agreement</div>
+    <div className="border p-2 h-full">
+      <h5>Certificate No.: {getValue(iD)}</h5>
+      <div className="text-center">
+        <div className="py-4">
+          <p className="font-bold">CERTIFICATE OF ORIGIN</p>
+          <p className="font-bold">Form for China-Australia Free Trade Agreement</p>
+        </div>
+        <p>
+          Issued in: <span className="font-bold underline">AUSTRALIA</span>
+        </p>
       </div>
-      <div>Issued in: AUSTRALIA</div>
     </div>
   );
 };
@@ -129,7 +135,8 @@ export const SummarySection: FunctionComponent<ChaftaCooDocument> = ({ iD }) => 
 export const OfficialUseSection: FunctionComponent = () => {
   return (
     <div className="border p-2 h-full">
-      <div>For official use only:</div>
+      <h5>For official use only:</h5>
+      <div style={{ minHeight: "80px" }} />
     </div>
   );
 };
@@ -144,18 +151,18 @@ export const ImporterSection: FunctionComponent<ChaftaCooDocumentWithObfuscation
   const privacyPath = ["supplyChainConsignment.importer"];
 
   return (
-    <div className="border p-2">
-      <div>
+    <div className="border p-2 h-full">
+      <h5>
         3. Importer’s name, address and country (if known):{" "}
         <PrivacyButton isPrivacyOn={isPrivacyOn} handleObfuscation={handleObfuscation} paths={privacyPath} />
-      </div>
-      <div>{importer?.name}</div>
-      <div>{postalAddress?.line1}</div>
-      <div>{postalAddress?.line2}</div>
-      <div>{postalAddress?.cityName}</div>
-      <div>
+      </h5>
+      <p>{importer?.name}</p>
+      <p>{postalAddress?.line1}</p>
+      <p>{postalAddress?.line2}</p>
+      <p>{postalAddress?.cityName}</p>
+      <p>
         {postalAddress?.countrySubDivisionName} {postalAddress?.postcode} {postalAddress?.countryCode}
-      </div>
+      </p>
     </div>
   );
 };
@@ -165,12 +172,20 @@ export const RemarksSection: FunctionComponent<ChaftaCooDocument> = ({ supplyCha
 
   return (
     <div className="border p-2 h-full">
-      <div>5. Remarks:</div>
-      <div>Consignment Ref: {getValue(supplyChainConsignment?.iD)}</div>
-      <div>{supplyChainConsignment?.information}</div>
-      {consignmentItems?.map((item, index) => {
-        return <div key={index}>- {item.information}</div>;
-      })}
+      <h5>5. Remarks:</h5>
+      {supplyChainConsignment?.iD && <p>Consignment Ref: {getValue(supplyChainConsignment?.iD)}</p>}
+      <p>{supplyChainConsignment?.information}</p>
+      {consignmentItems && consignmentItems?.length > 0 && (
+        <ul className="list-disc pl-4">
+          {consignmentItems?.map((item, index) => {
+            return (
+              <li key={index}>
+                <p>{item.information}</p>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 };
@@ -190,13 +205,15 @@ export const TransportSection: FunctionComponent<ChaftaCooDocumentWithObfuscatio
 
   return (
     <div className="border p-2 h-full">
-      <div>
-        4. Means of transport and route (if known){" "}
+      <h5>
+        4. Means of transport and route (if known):{" "}
         <PrivacyButton isPrivacyOn={isPrivacyOn} handleObfuscation={handleObfuscation} paths={privacyPath} />
-      </div>
-      <div>Departure Date: {printDate(departureEvent?.departureDateTime)}</div>
-      <div>Vessel/Flight/Train/Vehicle No.: {getValue(transportMovement?.usedTransportMeans?.iD)}</div>
-      <div>Port of loading: {getValue(loadingPortLocation?.iD)}</div>
+      </h5>
+      {departureEvent?.departureDateTime && <p>Departure Date: {printDate(departureEvent?.departureDateTime)}</p>}
+      {transportMovement?.usedTransportMeans?.iD && (
+        <p>Vessel/Flight/Train/Vehicle No.: {getValue(transportMovement?.usedTransportMeans?.iD)}</p>
+      )}
+      {loadingPortLocation?.iD && <p>Port of loading: {getValue(loadingPortLocation?.iD)}</p>}
     </div>
   );
 };
@@ -218,7 +235,7 @@ export const TradeLineItemsSection: FunctionComponent<ChaftaCooDocument> = ({ su
 
   consignmentItems?.forEach((consignmentItem) => {
     const { tradeLineItems } = consignmentItem;
-    tradeLineItems.forEach((tradeLineItem) => {
+    tradeLineItems?.forEach((tradeLineItem) => {
       let firstLineItem = true;
       const { transportPackages, tradeProduct } = tradeLineItem;
       transportPackages?.forEach((transportPackage) => {
@@ -243,25 +260,25 @@ export const TradeLineItemsSection: FunctionComponent<ChaftaCooDocument> = ({ su
   return (
     <div className="border-t border-b">
       <div className="flex flex-nowrap justify-center">
-        <div style={{ width: "10%" }} className="p-2 border-l border-r border-b-2">
+        <div style={{ width: "10%" }} className={`p-2 border-l border-r ${consignmentItems ? "border-b-2" : ""}`}>
           6. Item number (max. 20)
         </div>
-        <div style={{ width: "20%" }} className="p-2 border-l border-r border-b-2">
+        <div style={{ width: "20%" }} className={`p-2 border-l border-r ${consignmentItems ? "border-b-2" : ""}`}>
           7. Marks and numbers on packages (optional)
         </div>
-        <div style={{ width: "20%" }} className="p-2 border-l border-r border-b-2">
+        <div style={{ width: "20%" }} className={`p-2 border-l border-r ${consignmentItems ? "border-b-2" : ""}`}>
           8. Number and kind of packages; description of goods
         </div>
-        <div style={{ width: "10%" }} className="p-2 border-l border-r border-b-2">
+        <div style={{ width: "10%" }} className={`p-2 border-l border-r ${consignmentItems ? "border-b-2" : ""}`}>
           9. HS code (6 digit code)
         </div>
-        <div style={{ width: "10%" }} className="p-2 border-l border-r border-b-2">
+        <div style={{ width: "10%" }} className={`p-2 border-l border-r ${consignmentItems ? "border-b-2" : ""}`}>
           10. Origin criterion
         </div>
-        <div style={{ width: "15%" }} className="p-2 border-l border-r border-b-2">
+        <div style={{ width: "15%" }} className={`p-2 border-l border-r ${consignmentItems ? "border-b-2" : ""}`}>
           11. Gross or net weight or odiver quantity (e.g. Quantity Unit, litres, m³.)
         </div>
-        <div style={{ width: "15%" }} className="p-2 border-l border-r border-b-2">
+        <div style={{ width: "15%" }} className={`p-2 border-l border-r ${consignmentItems ? "border-b-2" : ""}`}>
           12. Invoice number and date
         </div>
       </div>
@@ -302,31 +319,31 @@ export const DeclarationSection: FunctionComponent<ChaftaCooDocument> = ({
   const importer = supplyChainConsignment?.importer;
 
   return (
-    <div className="border h-full">
-      <div className="p-2">
+    <div className="border p-2 h-full">
+      <h5>
         13. Declaration by the exporter or producer The undersigned hereby declares that the above-stated information is
         correct and that the goods exported to
+      </h5>
+      <div className="my-8 text-center">
+        <p>{importer?.name}</p>
+        <UnderlineDashed />
+        <p>(Importing Party)</p>
       </div>
-
-      <div className="text-center my-4">
-        <div>{importer?.name}</div>
-        <div className="border-b-2" />
-        <div>(Importing Party)</div>
-      </div>
-
-      <div className="p-2">
-        <div className="my-4">
-          comply with the origin requirements specified in the ChinaAustralia Free Trade Agreement.
-        </div>
-        <div className="my-2">
-          {supplyChainConsignment?.loadingBaseportLocation?.name},{" "}
+      <p>comply with the origin requirements specified in the ChinaAustralia Free Trade Agreement.</p>
+      <div className="my-4">
+        <p>
+          {supplyChainConsignment?.loadingBaseportLocation?.name}
+          {supplyChainConsignment?.loadingBaseportLocation?.name &&
+            firstSignatoryAuthentication?.actualDateTime &&
+            `, `}
           {printDate(firstSignatoryAuthentication?.actualDateTime)}
+        </p>
+        <div style={{ minHeight: "80px" }}>
+          <img className="w-1/2 mx-auto" data-testid="signature" src={firstSignatoryAuthentication?.signature} />
         </div>
-        <div className="w-1/2 mx-auto">
-          <img data-testid="signature" src={firstSignatoryAuthentication?.signature} />
-        </div>
-        <div>Place, date and signature of authorised person</div>
       </div>
+      <UnderlineDashed />
+      <p>Place, date and signature of authorised person</p>
     </div>
   );
 };
@@ -335,12 +352,12 @@ export const CertificationSection: FunctionComponent<ChaftaCooDocument> = ({ sec
   return (
     <div className="border p-2 h-full">
       <div className="flex flex-col h-full">
-        <div>14. Certification</div>
+        <h5>14. Certification</h5>
         <div className="flex-grow">
           <img className="w-1/2 mx-auto" src={secondSignatoryAuthentication?.signature} />
         </div>
-        <div className="border-t border-dashed mb-2" />
-        <div>Place, date and signature of authorised person</div>
+        <UnderlineDashed />
+        <p>Place, date and signature of authorised person</p>
       </div>
     </div>
   );
@@ -377,8 +394,10 @@ export const ChaftaCooTemplate: FunctionComponent<ChaftaCooTemplateProps> = (pro
         <div className="border">
           <div className="flex">
             <div className="w-1/2">
-              <ExporterSection {...documentData} handleObfuscation={handleObfuscation} isPrivacyOn={isPrivacyOn} />
-              <ProducerSection {...documentData} handleObfuscation={handleObfuscation} isPrivacyOn={isPrivacyOn} />
+              <div className="flex flex-col h-full">
+                <ExporterSection {...documentData} handleObfuscation={handleObfuscation} isPrivacyOn={isPrivacyOn} />
+                <ProducerSection {...documentData} handleObfuscation={handleObfuscation} isPrivacyOn={isPrivacyOn} />
+              </div>
             </div>
             <div className="w-1/2">
               <SummarySection {...documentData} />
