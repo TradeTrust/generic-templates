0ca6086d224bb3a5980cd022f1052af67579cf18
Sim Boon Long <simboonlong@gmail.com>
2022-02-22 16:13:57 +0800

    feat: tailwind v3

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { FramedDocumentRenderer, fullAttachmentRenderer } from "@govtechsg/decentralized-renderer-react-components";
 import { registry } from "./templates";
+import "./main.css";
 
 ReactDOM.render(
   <FramedDocumentRenderer templateRegistry={registry} attachmentToComponent={fullAttachmentRenderer} />,
