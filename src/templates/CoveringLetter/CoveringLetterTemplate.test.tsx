2ad9f991dfff1cd56cd80ad30b5c848ec1cd30a4
Sim Boon Long <simboonlong@gmail.com>
2023-07-17 15:01:01 +0800

    fix: test

--- src/templates/CoveringLetter/CoveringLetterTemplate.test.tsx
+++ src/templates/CoveringLetter/CoveringLetterTemplate.test.tsx
@@ -1,12 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
 import { CoveringLetterTemplate } from "./CoveringLetterTemplate";
-import { CoveringLetterSample } from "./sampleV2a";
-import { CoveringLetterSample2 } from "./sampleV2b";
+import { CoveringLetterSampleV2a } from "./sampleV2a";
+import { CoveringLetterSampleV2b } from "./sampleV2b";
 
 describe("covering Letter", () => {
   it("should render the cover letter (govtech) correctly", () => {
-    render(<CoveringLetterTemplate document={CoveringLetterSample} handleObfuscation={() => {}} />);
+    render(<CoveringLetterTemplate document={CoveringLetterSampleV2a} handleObfuscation={() => {}} />);
     expect(screen.getByText("Documents Bundle")).toBeInTheDocument();
     expect(screen.getByTestId("logo")).toHaveAttribute(
       "src",
@@ -15,7 +15,7 @@ describe("covering Letter", () => {
   });
 
   it("should render the cover letter (dbs) correctly", () => {
-    render(<CoveringLetterTemplate document={CoveringLetterSample2} handleObfuscation={() => {}} />);
+    render(<CoveringLetterTemplate document={CoveringLetterSampleV2b} handleObfuscation={() => {}} />);
     expect(screen.getByText("Documents Bundle")).toBeInTheDocument();
     expect(screen.getByTestId("logo")).toHaveAttribute("src", "test-file-stub");
   });
