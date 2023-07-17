16727485e625d3c7bb3b5f3e48ff18cc28ffcce0
Sim Boon Long <simboonlong@gmail.com>
2023-07-17 15:23:58 +0800

    fix: titles

--- src/integration/cover-letter-malformed.spec.ts
+++ src/integration/cover-letter-malformed.spec.ts
@@ -5,7 +5,7 @@ fixture("Cover Letter Malformed").page`http://localhost:3010`;
 const DefaultTemplate = Selector("#default-template");
 
 test("Covering Letter (Malformed) is rendered correctly", async (t) => {
-  await t.click(Selector("[data-testid='Covering Letter (Malformed)']"));
+  await t.click(Selector("[data-testid='Covering Letter V2 (Malformed)']"));
   await t.switchToIframe("#iframe");
   await t.expect(DefaultTemplate.visible).ok();
   await t
