4b1b431d5eb6f239aaa5073cef6410a73b7ff4c6
Lucas <lucas_yap@tech.gov.sg>
2023-08-28 16:14:46 +0800

    fix: change styles to classnames

--- src/templates/Brochure/BrochureTemplate.tsx
+++ src/templates/Brochure/BrochureTemplate.tsx
@@ -42,11 +42,11 @@ export const Page = styled.div`
 export const BrochureHeader: React.FC = () => (
   <>
     <div className="flex flex-row">
-      <img style={{ width: "150px" }} src={govtechLogo} />
+      <img className="w-36" src={govtechLogo} />
       <div className="grow"></div>
-      <img style={{ width: "150px" }} src={imdaLogo} />
+      <img className="w-36" src={imdaLogo} />
     </div>
-    <div style={{ height: "2px", width: "100%" }} className="bg-gradient-to-r from-green-700 to-sky-400 my-4"></div>
+    <div className="h-0.5 w-full bg-gradient-to-r from-green-700 to-sky-400 my-4"></div>
   </>
 );
 
@@ -76,10 +76,7 @@ export const BrochurePage: React.FC<BrochurePageProps> = ({
         <div className="relative text-zinc-500">
           <img src={logo} className="mb-4" />
           {description}
-          <div
-            style={{ height: "100%", width: "2px" }}
-            className="absolute -right-4 top-0 bg-gradient-to-b from-green-700 to-sky-400"
-          ></div>
+          <div className="h-full w-0.5 absolute -right-4 top-0 bg-gradient-to-b from-green-700 to-sky-400"></div>
         </div>
         {footnote && (
           <div className="text-sm text-zinc-500">
@@ -95,153 +92,182 @@ export const BrochurePage: React.FC<BrochurePageProps> = ({
   </Page>
 );
 
-export const BrochureTemplate: FunctionComponent<TemplateProps<BrochureSchema>> = ({ document }) => {
-  const documentData = getDocumentData(document) as BrochureDocument;
+const Page1: React.FC<{ document: BrochureDocument }> = ({ document }) => {
+  const contents = document.page1.contents;
   return (
-    <Wrapper data-testid="brochure-template">
-      <BrochurePage
-        description={documentData.shared.tradeTrust.description}
-        page={1}
-        logo={documentData.shared.tradeTrust.logo}
-        footnote={documentData.page1.footnote}
-        header={documentData.page1.header}
-      >
-        {documentData.page1.contents.map((section, i) => {
-          if (i === documentData.page1.contents.length - 1)
+    <BrochurePage
+      description={document.shared.tradeTrust.description}
+      page={1}
+      logo={document.shared.tradeTrust.logo}
+      footnote={document.page1.footnote}
+      header={document.page1.header}
+    >
+      {contents.map((section, i) => {
+        if (i === contents.length - 1)
+          return (
+            <>
+              <h5>{section.subheader}</h5>
+              {section.bodyAsList?.[0]}
+              <b>{section.bolded?.[0]}</b>
+              {section.bodyAsList?.[1]}
+              <b>{section.bolded?.[1]}</b>
+              {section.bodyAsList?.[2]}
+            </>
+          );
+        else {
+          return (
+            <>
+              <h5>{section.subheader}</h5>
+              <div>{section.body}</div>
+              <br />
+            </>
+          );
+        }
+      })}
+    </BrochurePage>
+  );
+};
+
+const Page2: React.FC<{ document: BrochureDocument }> = ({ document }) => {
+  const contents = document.page2.contents;
+  return (
+    <BrochurePage
+      description={document.shared.openAttestation.description}
+      page={2}
+      logo={document.shared.openAttestation.logo}
+    >
+      <h5>{contents[0].subheader}</h5>
+      <div>{contents[0].body}</div>
+      <br />
+      <div>{contents[1].body}</div>
+      <br />
+      <ul className="list-disc ml-4">
+        {contents[1].listItems?.map((item) => (
+          <>
+            <li className="ml-4">
+              <b>{item.name} </b>
+              {item.description}
+            </li>
+            <br />
+          </>
+        ))}
+      </ul>
+      <div>
+        {contents[2].bodyAsList?.[0]}
+        <b>{contents[2].bolded?.[0]}</b>
+        {contents[2].bodyAsList?.[1]}
+        <b>{contents[2].bolded?.[1]}</b>
+        {contents[2].bodyAsList?.[2]}
+      </div>
+      <br />
+      <h5>{contents[3].subheader}</h5>
+      <div>
+        {contents[3].body}
+        <a href={contents[3].link} target="_blank">
+          {contents[3].link}
+        </a>
+        .
+      </div>
+    </BrochurePage>
+  );
+};
+
+const Page3: React.FC<{ document: BrochureDocument }> = ({ document }) => {
+  const contents = document.page3.contents;
+  return (
+    <BrochurePage
+      description={document.shared.openAttestation.description}
+      page={3}
+      logo={document.shared.openAttestation.logo}
+    >
+      <h5>{contents[0].subheader}</h5>
+      <div>{contents[0].body}</div>
+      <br />
+      <ul className="list-decimal ml-8">
+        {contents[1].listItems?.map((item, i) => {
+          if (i === 0) {
             return (
               <>
-                <h5>{section.subheader}</h5>
-                {section.bodyAsList?.[0]}
-                <b>{section.bolded?.[0]}</b>
-                {section.bodyAsList?.[1]}
-                <b>{section.bolded?.[1]}</b>
-                {section.bodyAsList?.[2]}
+                <li>
+                  <b>{item.name} </b>
+                </li>
+                {item.descriptionAsList?.map((part) => (
+                  <>
+                    <div>{part}</div>
+                    <br />
+                  </>
+                ))}
               </>
             );
-          else {
+          } else {
             return (
               <>
-                <h5>{section.subheader}</h5>
-                <div>{section.body}</div>
+                <li>
+                  <b>{item.name} </b>
+                </li>
+                <div>{item.description}</div>
                 <br />
               </>
             );
           }
         })}
-      </BrochurePage>
-      <BrochurePage
-        description={documentData.shared.openAttestation.description}
-        page={2}
-        logo={documentData.shared.openAttestation.logo}
-      >
-        <h5>{documentData.page2.contents[0].subheader}</h5>
-        <div>{documentData.page2.contents[0].body}</div>
-        <br />
-        <div>{documentData.page2.contents[1].body}</div>
-        <br />
-        <ul className="list-disc ml-4">
-          {documentData.page2.contents[1].listItems?.map((item) => (
-            <>
-              <li className="ml-4">
-                <b>{item.name} </b>
-                {item.description}
-              </li>
-              <br />
-            </>
-          ))}
-        </ul>
-        <div>
-          {documentData.page2.contents[2].bodyAsList?.[0]}
-          <b>{documentData.page2.contents[2].bolded?.[0]}</b>
-          {documentData.page2.contents[2].bodyAsList?.[1]}
-          <b>{documentData.page2.contents[2].bolded?.[1]}</b>
-          {documentData.page2.contents[2].bodyAsList?.[2]}
-        </div>
-        <br />
-        <h5>{documentData.page2.contents[3].subheader}</h5>
-        <div>
-          {documentData.page2.contents[3].body}
-          <a href={documentData.page2.contents[3].link} target="_blank">
-            {documentData.page2.contents[3].link}
-          </a>
-          .
-        </div>
-      </BrochurePage>
-      <BrochurePage
-        description={documentData.shared.openAttestation.description}
-        page={3}
-        logo={documentData.shared.openAttestation.logo}
-      >
-        <h5>{documentData.page3.contents[0].subheader}</h5>
-        <div>{documentData.page3.contents[0].body}</div>
-        <br />
-        <ul className="list-decimal ml-8">
-          {documentData.page3.contents[1].listItems?.map((item, i) => {
-            if (i === 0) {
-              return (
+      </ul>
+    </BrochurePage>
+  );
+};
+
+const Page4: React.FC<{ document: BrochureDocument }> = ({ document }) => {
+  const contents = document.page4.contents;
+  const footer = document.page4.footer;
+  return (
+    <BrochurePage
+      description={document.shared.openAttestation.description}
+      page={4}
+      logo={document.shared.openAttestation.logo}
+      footer={
+        <>
+          <div className="h-0.5 w-full bg-gradient-to-r from-green-700 to-sky-400 my-4 mt-72 mb-2" />
+          <div className="flex flex-row items-center h-fit w-full bg-gradient-to-r from-green-700 to-sky-400 rounded-lg p-7">
+            <img src={footer.qrCode} className="h-32 w-32" />
+            <div className="w-48 ml-4 text-white text-sm">{footer.qrPrompt}</div>
+            <div className="grow" />
+            <div className="flex flex-col text-right text-white text-sm">
+              {footer.links.map((item, i) => (
                 <>
-                  <li>
-                    <b>{item.name} </b>
-                  </li>
-                  {item.descriptionAsList?.map((part) => (
-                    <>
-                      <div>{part}</div>
-                      <br />
-                    </>
+                  <b className={i === 0 ? "mb-2" : "mt-4 mb-2"}>{item.title}</b>
+                  {item.domains.map((link) => (
+                    <div>{link}</div>
                   ))}
                 </>
-              );
-            } else {
-              return (
-                <>
-                  <li>
-                    <b>{item.name} </b>
-                  </li>
-                  <div>{item.description}</div>
-                  <br />
-                </>
-              );
-            }
-          })}
-        </ul>
-      </BrochurePage>
-      <BrochurePage
-        description={documentData.shared.openAttestation.description}
-        page={4}
-        logo={documentData.shared.openAttestation.logo}
-        footer={
-          <>
-            <div className="h-0.5 w-full bg-gradient-to-r from-green-700 to-sky-400 my-4 mt-72 mb-2" />
-            <div className="flex flex-row items-center h-fit w-full bg-gradient-to-r from-green-700 to-sky-400 rounded-lg p-7">
-              <img src={documentData.page4.footer.qrCode} className="h-32 w-32" />
-              <div className="w-48 ml-4 text-white text-sm">{documentData.page4.footer.qrPrompt}</div>
-              <div className="grow" />
-              <div className="flex flex-col text-right text-white text-sm">
-                {documentData.page4.footer.links.map((item, i) => (
-                  <>
-                    <b className={i === 0 ? "mb-2" : "mt-4 mb-2"}>{item.title}</b>
-                    {item.domains.map((link) => (
-                      <div>{link}</div>
-                    ))}
-                  </>
-                ))}
-              </div>
+              ))}
             </div>
+          </div>
+        </>
+      }
+    >
+      <h5>{contents[0].subheader}</h5>
+      <div>{contents[0].body}</div>
+      <br />
+      <ul className="list-disc ml-8">
+        {contents[1].listItems?.map((item) => (
+          <>
+            <li>{item.description}</li>
+            <br />
           </>
-        }
-      >
-        <h5>{documentData.page4.contents[0].subheader}</h5>
-        <div>{documentData.page4.contents[0].body}</div>
-        <br />
-        <ul className="list-disc ml-8">
-          {documentData.page4.contents[1].listItems?.map((item) => (
-            <>
-              <li>{item.description}</li>
-              <br />
-            </>
-          ))}
-        </ul>
-      </BrochurePage>
+        ))}
+      </ul>
+    </BrochurePage>
+  );
+};
+
+export const BrochureTemplate: FunctionComponent<TemplateProps<BrochureSchema>> = ({ document }) => {
+  const documentData = getDocumentData(document) as BrochureDocument;
+  return (
+    <Wrapper data-testid="brochure-template">
+      <Page1 document={documentData} />
+      <Page2 document={documentData} />
+      <Page3 document={documentData} />
+      <Page4 document={documentData} />
     </Wrapper>
   );
 };
