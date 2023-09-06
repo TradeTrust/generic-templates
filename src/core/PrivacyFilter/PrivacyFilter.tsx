198bb14f1c5b553334d336f03849861741484aaa
Lucas <31716292+yapyuyou@users.noreply.github.com>
2023-09-06 17:49:36 +0800

    chore: add w3c brochure mobile view (#86)
    * chore: add mobile view
    
    * feat: add fonts (#88)
    
    ---------
    
    Co-authored-by: Sim Boon Long <simboonlong@gmail.com>

--- src/core/PrivacyFilter/PrivacyFilter.tsx
+++ src/core/PrivacyFilter/PrivacyFilter.tsx
@@ -5,30 +5,37 @@ import patternWaves from "/static/images/pattern-waves.png";
 interface PrivacyFilterProps {
   editable: boolean;
   onToggleEditable: () => void;
-  className?: string;
+  options?: {
+    className?: string;
+    description?: string;
+    buttonText?: string;
+  };
 }
 
-export const PrivacyFilter: FunctionComponent<PrivacyFilterProps> = ({ editable, onToggleEditable, className }) => (
-  <div
-    className={className ? className : "print:hidden bg-cover bg-cerulean text-white rounded-lg p-4 mb-8"}
-    style={{ backgroundImage: `url(${patternWaves})` }}
-  >
-    <div className="container">
-      <div className="md:flex items-center">
-        <div className="grow mb-4 md:mb-0 mr-0 md:mr-4">
-          <h3 className="font-normal">The document allows fields to be selectively disclosed.</h3>
-          <p>
-            Remove sensitive information on this document by clicking on the edit button. Downloaded document remains
-            valid.
-          </p>
+export const PrivacyFilter: FunctionComponent<PrivacyFilterProps> = ({ editable, onToggleEditable, options }) => {
+  const defaultOptions = {
+    className: "print:hidden bg-cover bg-cerulean text-white rounded-lg p-4 mb-8",
+    description: `Remove sensitive information on this document by clicking on the edit button. Downloaded document remains valid.`,
+    buttonText: "Edit Document",
+  };
+  const { className, description, buttonText } = options ?? defaultOptions;
+
+  return (
+    <div className={className} style={{ backgroundImage: `url(${patternWaves})` }}>
+      <div className="container">
+        <div className="md:flex items-center">
+          <div className="grow mb-4 md:mb-0 mr-0 md:mr-4">
+            <h3 className="font-normal">The document allows fields to be selectively disclosed.</h3>
+            <p>{description}</p>
+          </div>
+          <Button onClick={onToggleEditable} className="bg-white text-cerulean hover:bg-gray-50 whitespace-nowrap">
+            {editable ? "Done" : buttonText}
+          </Button>
         </div>
-        <Button onClick={onToggleEditable} className="bg-white text-cerulean hover:bg-gray-50 whitespace-nowrap">
-          {editable ? "Done" : "Edit Document"}
-        </Button>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export const IconRedact: FunctionComponent = () => {
   return (
