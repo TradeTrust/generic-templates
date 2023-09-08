b22f90d012c8b740d2e0da9f523cb40a4d70c53e
Lucas <lucas_yap@tech.gov.sg>
2023-09-08 15:51:57 +0800

    chore: upgrade deps and types

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 import { OpenAttestationDocument, utils } from "@govtechsg/open-attestation";
 
 export const getDocumentData = (document: OpenAttestationDocument): any => {
-  return utils.isRawV3Document(document) ? document.credentialSubject : document;
+  return utils.isRawV3Document(document) || utils.isRawV4Document(document) ? document.credentialSubject : document;
 };
