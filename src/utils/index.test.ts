04b249039c55abe67950e7d3bb4714f67a0c8439
cavacado <ngzhaoloong@gmail.com>
2023-11-14 12:18:59 +0800

    fix: lint errors

--- src/utils/index.test.ts
+++ src/utils/index.test.ts
@@ -1,6 +1,6 @@
 import { ChaftaCooSampleV2 } from "./../templates/ChaftaCoo/sampleV2";
 import { ChaftaCooSampleV3 } from "./../templates/ChaftaCoo/sampleV3";
-import { InvoiceSampleV4 } from "./../templates/Invoice/sampleV4"
+import { InvoiceSampleV4 } from "./../templates/Invoice/sampleV4";
 import { getDocumentData } from "./";
 
 describe("getDocumentData", () => {
