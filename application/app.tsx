e1845e10b5bbecc9c173f2d31db8dd69952a55ed
cavacado <ngzhaoloong@gmail.com>
2023-11-21 15:08:55 +0800

    chore: lint fix

--- application/app.tsx
+++ application/app.tsx
@@ -1,4 +1,8 @@
-import { FrameActions, FrameConnector, HostActionsHandler } from "@tradetrust-tt/decentralized-renderer-react-components";
+import {
+  FrameActions,
+  FrameConnector,
+  HostActionsHandler,
+} from "@tradetrust-tt/decentralized-renderer-react-components";
 import React, { useCallback, useEffect, useState } from "react";
 
 interface AppProps {
