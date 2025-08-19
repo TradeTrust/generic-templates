f7727484530dc5de5e4df68d33af27d02488a9f1
Rishabh Singh <rishabh7singh5@gmail.com>
2025-08-19 15:39:32 +0530

    fix: resolve import error

--- jest.config.js
+++ jest.config.js
@@ -16,4 +16,7 @@ module.exports = {
   setupFilesAfterEnv: ["./jest.setup.ts"],
   testEnvironment: "jest-environment-jsdom",
   testRegex: "(/__tests__/.*|(\\.|/)(test))\\.tsx?$",
+  transformIgnorePatterns: [
+    "node_modules/(?!(@digitalbazaar|@trustvc|@mattrglobal|base64url-universal|base58-universal|cborg|multiformats|uint8arrays)/)",
+  ],
 };
